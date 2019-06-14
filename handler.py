25aba7e7fb94ddd1a9f418b798fa83cdb6b6175b
RyanKilbane <RyanKilbane92@hotmail.com>
2019-06-14 11:58:26 +0100

    changed the name of handler function

--- handler.py
+++ handler.py
@@ -2,7 +2,7 @@ import json
 from runner import Runner
 
 
-def hello(event, context):
+def run_validation_rule(event, context):
     rule = Runner(event)
     rule.load_data()
     return rule.run_rule()
